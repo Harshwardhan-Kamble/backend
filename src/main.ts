189f89962b3fdbf10fab3317c6b2faef4cc5a246
Harshwardhan-Kamble <harshwardhankamble2004@gmail.com>
2025-08-28 19:31:12 +0530

    feat: add serverless express and AWS Lambda dependencies; update main.ts to use environment port; create Vercel configuration

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   // enable validation globally
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
